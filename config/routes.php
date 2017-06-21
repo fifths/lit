27329218a4f7dfd12668a9ad3f84e93d04b3c5c6
fifths <ligangmingx@gmail.com>
2017-06-21 22:58:03 +0800

    update router

--- config/routes.php
+++ config/routes.php
@@ -1,20 +1,20 @@
 <?php
-use \NoahBuscher\Macaw\Macaw;
+use Router\Route;
 
-Macaw::get('/', function () {
+Route::get('/', function () {
     echo 'Hello world!';
 });
 
-Macaw::get('/home', 'HomeController@home');
-Macaw::get('/test', 'HomeController@test');
+Route::get('/home', 'HomeController@home');
+Route::get('/test', 'HomeController@test');
 /*Macaw::error(function () {
     echo '404 :: Not Found';
 });*/
 
-Macaw::$error_callback = function() {
+Route::$error_callback = function() {
 
     throw new Exception("路由无匹配项 404 Not Found");
 
 };
 
-Macaw::dispatch();
+Route::dispatch();
